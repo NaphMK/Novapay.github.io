5000f628d395399f88e2622fc0c51318a932cff5
NaphMK <148817115+NaphMK@users.noreply.github.com>
2025-02-26 15:18:02 -0800

    add

--- backend/api/monnify_accept.php
+++
@@ -1,7 +0,0 @@
-<?php
-$data = file_get_contents("php://input");
-
-$json_data = json_decode($data, true);
-
-
-?>
--- backend/config/testdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'C:\xampp\htdocs\vtu\k-wd-dashboard\backend\config\db.php';
-
-if ($pdo) {
-    echo "Database connection successful!";
-} else {
-    echo "Database connection failed!";
-}
-?>
